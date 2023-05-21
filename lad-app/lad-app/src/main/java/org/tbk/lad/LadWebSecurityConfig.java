8daa7c26f30e5878c142103d41b3c7c1f688c4c6
theborakompanioni <theborakompanioni+github@gmail.com>
2023-05-21 16:00:27 +0200

    build(deps): update spring-boot to v3.1.0

--- lad-app/lad-app/src/main/java/org/tbk/lad/LadWebSecurityConfig.java
+++ lad-app/lad-app/src/main/java/org/tbk/lad/LadWebSecurityConfig.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -15,41 +14,35 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Slf4j
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
-class LadWebSecurityConfig implements WebSecurityCustomizer {
-
-    @Override
-    public void customize(WebSecurity web) {
-        web
-                .ignoring()
-        ;
-    }
-
+class LadWebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        antMatcher("/.well-known/lnurlp/**"),
-                        antMatcher("/api/v1/lnurl/pay/**")
-                ).permitAll()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        antMatcher("/"),
-                        antMatcher("/index.html"),
-                        antMatcher("/fonts/**"),
-                        PathRequest.toStaticResources().atCommonLocations()
-                ).permitAll()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        antMatcher("/swagger-ui.html"),
-                        antMatcher("/swagger-ui/**"),
-                        antMatcher("/v3/api-docs/swagger-config"),
-                        antMatcher("/v3/api-docs")
-                ).permitAll();
+                .cors(cors -> {
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(
+                            antMatcher("/.well-known/lnurlp/**"),
+                            antMatcher("/api/v1/lnurl/pay/**")
+                    ).permitAll();
+                })
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(
+                            antMatcher("/"),
+                            antMatcher("/index.html"),
+                            antMatcher("/fonts/**"),
+                            PathRequest.toStaticResources().atCommonLocations()
+                    ).permitAll();
+                })
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(
+                            antMatcher("/swagger-ui.html"),
+                            antMatcher("/swagger-ui/**"),
+                            antMatcher("/v3/api-docs/swagger-config"),
+                            antMatcher("/v3/api-docs")
+                    ).permitAll();
+                });
 
         return http.build();
     }
