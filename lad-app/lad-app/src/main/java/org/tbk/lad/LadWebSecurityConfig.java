980dd1f4f678e11e63a499c3cf48a94cb92cbae2
theborakompanioni <theborakompanioni+github@gmail.com>
2023-07-02 09:45:02 +0200

    chore(security): require authentication for non-public api endpoints

--- lad-app/lad-app/src/main/java/org/tbk/lad/LadWebSecurityConfig.java
+++ lad-app/lad-app/src/main/java/org/tbk/lad/LadWebSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,11 +16,11 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 class LadWebSecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> {
-                })
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(
@@ -42,6 +43,9 @@ class LadWebSecurityConfig {
                             antMatcher("/v3/api-docs/swagger-config"),
                             antMatcher("/v3/api-docs")
                     ).permitAll();
+                })
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
                 });
 
         return http.build();
