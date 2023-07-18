e78e0f967604d2cc9227f38a26d2b3371e6fd8ae
theborakompanioni <theborakompanioni+github@gmail.com>
2023-07-18 11:52:22 +0200

    fix: parse any character between the separators as comment

--- lad-app/lad-app/src/main/java/org/tbk/lad/lnaddress/service/DefaultLnAddressResolver.java
+++ lad-app/lad-app/src/main/java/org/tbk/lad/lnaddress/service/DefaultLnAddressResolver.java
@@ -1,6 +1,7 @@
 package org.tbk.lad.lnaddress.service;
 
 import jakarta.validation.ConstraintViolationException;
+import lombok.NonNull;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.tbk.lad.lnaddress.spi.LnAddressResolver;
 import org.tbk.lad.lnaddress.spi.dto.LnAddressParts;
@@ -10,7 +11,7 @@ import java.util.Optional;
 
 public class DefaultLnAddressResolver implements LnAddressResolver {
     @Override
-    public LnAddressParts resolveLnAddressParts(String raw) {
+    public LnAddressParts resolveLnAddressParts(@NonNull String raw) {
         if (!raw.contains("@")) {
             throw new ConstraintViolationException("Invalid address", Collections.emptySet());
         }
@@ -19,31 +20,31 @@ public class DefaultLnAddressResolver implements LnAddressResolver {
         }
 
         int atSignIndex = raw.lastIndexOf("@");
-        int lastPlusSignIndex = raw.lastIndexOf("+");
-
-        String username = (lastPlusSignIndex <= 0 ? raw.substring(0, atSignIndex) : raw.substring(0, lastPlusSignIndex))
-                .replace(".", "");
-
-        String commentOrNull = Optional.of(lastPlusSignIndex <= 0 ? "" : raw.substring(lastPlusSignIndex + 1, atSignIndex))
-                .filter(String::isBlank)
-                .orElse(null);
-        String domain = raw.substring(atSignIndex + 1);
-
-        boolean isTor = domain.endsWith(".onion");
-        String scheme = isTor || domain.startsWith("localhost:") ? "http" : "https";
-
-        String sanitizedDomain = UriComponentsBuilder.fromHttpUrl("%s://%s".formatted(scheme, domain))
-                .replacePath(null)
-                .replaceQuery(null)
-                .fragment(null)
-                .build()
-                .normalize()
-                .toUri()
-                .getAuthority();
-
-        if ("".equals(username) || sanitizedDomain.length() <= 2) {
+        int firstPlusSignIndex = raw.indexOf("+");
+        if (firstPlusSignIndex > atSignIndex) {
             throw new ConstraintViolationException("Invalid address", Collections.emptySet());
         }
+
+        String username = (firstPlusSignIndex <= 0 ? raw.substring(0, atSignIndex) : raw.substring(0, firstPlusSignIndex))
+                .replace(".", "");
+        if (username.isBlank()) {
+            throw new ConstraintViolationException("Invalid address", Collections.emptySet());
+        }
+
+        String domain = raw.substring(atSignIndex + 1);
+        if (domain.isBlank()) {
+            throw new ConstraintViolationException("Invalid address", Collections.emptySet());
+        }
+
+        String commentOrNull = Optional.of(firstPlusSignIndex <= 0 ? "" : raw.substring(firstPlusSignIndex + 1, atSignIndex))
+                .filter(it -> !it.isBlank())
+                .orElse(null);
+
+        boolean isTor = domain.endsWith(".onion");
+        boolean isLocal = "localhost".equals(domain) || domain.startsWith("localhost:");
+        String scheme = isTor || isLocal ? "http" : "https";
+
+        String sanitizedDomain = sanitizeDomain(domain, scheme);
 
         return LnAddressParts.builder()
                 .raw(raw)
@@ -53,5 +54,22 @@ public class DefaultLnAddressResolver implements LnAddressResolver {
                 .domain(sanitizedDomain)
                 .isTor(isTor)
                 .build();
+    }
+
+    private static String sanitizeDomain(String domain, String scheme) {
+        String sanitizedDomain = UriComponentsBuilder.fromHttpUrl("%s://%s".formatted(scheme, domain))
+                .replacePath(null)
+                .replaceQuery(null)
+                .fragment(null)
+                .build()
+                .normalize()
+                .toUri()
+                .getAuthority();
+
+        if (sanitizedDomain.length() <= 2) {
+            throw new ConstraintViolationException("Invalid address", Collections.emptySet());
+        }
+
+        return sanitizedDomain;
     }
 }
